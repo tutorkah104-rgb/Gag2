e0122081d4796394076c9eeebf30d74c5899a608
tutorkah104-rgb <tutorkah104@gmail.com>
2026-07-27 19:25:00 +0700

    Update Idk.lua

--- Idk.lua
+++ Idk.lua
@@ -1,5 +1,1 @@
-if id == 1 then
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/mQUlnr6tNmy5TYn8/raw"))()
-elseif id == 2 then print"tes"
-else error"Did you forget to set id?"
-end
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/YSYxCfurMk3jHrKr/raw"))()
