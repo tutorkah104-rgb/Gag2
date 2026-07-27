cad0c0889a6ebcc6f9858794e44aac3626acdac8
tutorkah104-rgb <tutorkah104@gmail.com>
2026-07-27 20:47:11 +0700

    Update Idk.lua

--- Idk.lua
+++ Idk.lua
@@ -1,7 +1,7 @@
 if not id or id == nil then
   loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/sFQgQIyWrLu9YLOT/raw"))()
 elseif id == "FREEVER" then
-  loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/ml4LZLmAsOwuU5Ry/raw"))()
+  loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/y4hXxgG66cATnq6I/raw"))()
 else 
   error"Unexpected error" 
 end
