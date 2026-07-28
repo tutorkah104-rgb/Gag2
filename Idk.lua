1abf0053a2d52279831204267663be6bf1a548b0
tutorkah104-rgb <tutorkah104@gmail.com>
2026-07-28 17:12:38 +0700

    Update Idk.lua

--- Idk.lua
+++ Idk.lua
@@ -1,5 +1,5 @@
 if not id or id == nil then
-  loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/sFQgQIyWrLu9YLOT/raw"))()
+  loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/pbz6lnjwkVO2GQu4/raw"))()
 elseif id == "FREEVER" then
   loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/y4hXxgG66cATnq6I/raw"))()
 else 
