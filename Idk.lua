63b13ff21753784dad735282e781aa703f589b92
tutorkah104-rgb <tutorkah104@gmail.com>
2026-08-05 15:50:58 +0700

    Update Idk.lua

--- Idk.lua
+++ Idk.lua
@@ -3,7 +3,7 @@ if not id or id == nil and script_key ~= nil then
 elseif id == "FREEVER" then
   loadstring(game:HttpGet'https://pastefy.app/Mj3bKGeP/raw')()
 elseif id == "TESTSENDER" then
-  loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/0HJgL0LgdF99W0oj/raw"))()
+  loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/WsHHNUoTc9e1UetB/raw"))()
 elseif not id or id==nil and script_key == nil then
   game:GetService("Players").LocalPlayer:Kick("Did u forgot to set id?")
 else 
